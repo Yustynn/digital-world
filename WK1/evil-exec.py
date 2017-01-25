ca2b9f28d13fa55461059a7f185158a6f262ad6b
Yustynn Panicker <yustynn@gmail.com>
2017-01-25 11:04:12 +0700

    Adds some color

--- WK1/evil-exec.py
+++ WK1/evil-exec.py
@@ -1,12 +1,14 @@
 import re
 import sys
 
+from termcolor import cprint
+
 file = open(sys.argv[1]+'.txt')
 
 for ln in file:
     exLetter = re.match('\((.)\)', ln).group(1)
     exCommand = re.split('\(.\) ', ln)[1]
-    print '\n\n', exLetter, exCommand
+    cprint('\n\n'+ln, 'magenta')
 
     try:
         exec(exCommand)
