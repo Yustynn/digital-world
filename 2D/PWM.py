de857f9a0e915ff5016a71490774f8d5f2acfc0b
Yustynn Panicker <yustynn@gmail.com>
2017-04-02 19:20:42 +0800

    Try again

--- 2D/PWM.py
+++ 2D/PWM.py
@@ -8,23 +8,30 @@ IO.setmode(IO.BCM)
 
 IO.setup(18, IO.OUT)
 
-class PWM(object):
-    def __init__(self, channel):
-        IO.setup(channel, IO.OUT)
-        self.pwm = IO.PWM(channel, 100)
+class Channel(object):
+    def __init__(self, pin):
+        self.pin = pin
+
+        IO.setup(pin, IO.OUT)
+        self.pwm = IO.PWM(pin, 50)
+
+class PWM(Channel):
+    def __init__(self, pin):
+        super(PWM, self).__init__(pin)
         self.pwm.start(0)
 
     def set_power(self, power):
         self.pwm.ChangeDutyCycle(power * 100)
         print 'power set to '+str(power)
 
-class WaterPump(PWM):
-    def __init__(self, channel1, channel2):
-        self.pwms = [PWM(channel) for channel in [channel1, channel2]]
+p = PWM(18)
+p.start(0)
+
+class WaterPump(object):
+    def __init__(self, pin1, pin2):
+        self.channels = [Channel(pin) for pin in [pin1, pin2]]
+        IO.output(self.channels[0].pin, True)
+        IO.output(self.channels[1].pin, False)
 
     def set_power(self, power):
-        self.pwms.set_power(power)
-
-class Fan(PWM):
-    def __init__(self, channel):
-        super(WaterPump, self).__init__(channel)
+        p.set_power(power)
