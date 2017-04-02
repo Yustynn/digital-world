3a8d700d5518de629f1195880d1df09cf2eabe7d
Yustynn Panicker <yustynn@gmail.com>
2017-04-02 18:51:51 +0800

    Alter structure of WaterPump

--- 2D/PWM.py
+++ 2D/PWM.py
@@ -4,7 +4,7 @@ IO.setwarnings(False)
 
 # tells it to use GPIO numbers instead of PIN NUMBERs
 # e.g. 19 for GPIO19 instead of 35 for PIN35 aka GPIO19
-IO.setmode(IO.BCM) 
+IO.setmode(IO.BCM)
 
 IO.setup(18, IO.OUT)
 
@@ -15,13 +15,15 @@ class PWM(object):
         self.pwm.start(0)
 
     def set_power(self, power):
-        dc = int( power*100//1 )
-        self.pwm.ChangeDutyCycle(dc)
-        print 'power set to '+str(dc)
+        self.pwm.ChangeDutyCycle(power * 100)
+        print 'power set to '+str(power)
 
 class WaterPump(PWM):
-    def __init__(self, channel):
-        super(WaterPump, self).__init__(channel)
+    def __init__(self, channel1, channel2):
+        self.pwms = [PWM(channel) for channel in [channel1, channel2]]
+
+    def set_power(self, power):
+        self.pwms.set_power(power)
 
 class Fan(PWM):
     def __init__(self, channel):
