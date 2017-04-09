a69029a46aef09e8cdd3fc1f69c52689b22f7342
Yustynn Panicker <yustynn@gmail.com>
2017-04-09 15:26:17 +0800

    Finish basic layout, do a little firebase

--- 2D/gui/__main__.py
+++ 2D/gui/__main__.py
@@ -1,18 +1,29 @@
 import kivy
 
-from kivy.app            import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.slider     import Slider
+from kivy.app           import App
+from kivy.clock         import Clock
+from kivy.properties    import NumericProperty   
+from kivy.uix.widget    import Widget
+
+#from logic.fb import get_temp
 
 kivy.require('1.9.1')
 
-class RootWidget():
-    pass
-
-class IdealTempSlider(Slider):
+class RootWidget(Widget):
     def __init__(self):
-        super(IdealTempSlider, self).__init__()
-        print self.background_width
+        super(RootWidget, self).__init__()
+
+        self.ideal_temp = 30.0
+        self.power      = 0
+        self.temp       = NumericProperty(30.0)
+
+
+    def update(self):
+        self.temp = get_temp()
+#        self.update()
+
+class CurrentData(Widget):
+    pass
 
 class GUIApp(App):
     build = lambda s: RootWidget()
