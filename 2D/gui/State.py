4a70b7b147265f745f9b7f7cb998a17f27b193e6
Yustynn Panicker <yustynn@gmail.com>
2017-04-09 17:50:25 +0800

    Modularize

--- 2D/gui/State.py
+++ 2D/gui/State.py
@@ -1,9 +1,9 @@
 from kivy.uix.widget    import Widget
 from kivy.properties    import NumericProperty, ObjectProperty
 
-
 # other mods
 from threading          import Thread
+from functools          import update_wrapper
 
 # self-created mods
 import logic.fb as fb
@@ -15,9 +15,13 @@ def unblock(fn):
     t.start()
 
 class State(Widget):
-    ideal_temp = NumericProperty(fb.get('ideal_temperature'))
-    power      = NumericProperty(fb.get('power'))
-    temp       = NumericProperty(fb.get('temperature'))
+    # ideal_temp = NumericProperty(fb.get('ideal_temperature'))
+    # power      = NumericProperty(fb.get('power'))
+    # temp       = NumericProperty(fb.get('temperature'))
+
+    ideal_temp = NumericProperty(0.0)
+    power      = NumericProperty(0.0)
+    temp       = NumericProperty(0.0)
 
     def __init__(self, ideal_temp = 30.0, power = 0.0):
         Widget.__init__(self)
--- 2D/gui/__main__.py
+++ 2D/gui/__main__.py
@@ -6,11 +6,13 @@ from kivy.clock         import Clock
 from kivy.properties    import ObjectProperty
 from kivy.uix.widget    import Widget
 
+from State import state
+
 ### SETUP ###
 kivy.require('1.9.1') # could propably go even earlier, but just in case
 
 class RootWidget(Widget):
-    state = ObjectProperty(None)
+    state = ObjectProperty(state)
 
 class CurrentData(Widget):
     pass
