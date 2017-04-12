bda2cbe909223d4990c22fd61b7c5d73f77407b8
Yustynn Panicker <yustynn@gmail.com>
2017-04-12 11:40:45 +0800

    Update controller algo

--- 2D/laptop/logic/Controller.py
+++ 2D/laptop/logic/Controller.py
@@ -15,9 +15,14 @@ opposite direction to the proportional controller, acts to 'slow down' the desce
 of the temperature, thereby reducing the overshoot.
 
 # Why Those k Values?
-Taken from 1st order in math. kp = k2-k1, and kd = k1.
+From the optimization problem we solved in math, these k values best
+minimize the overall error.
 
-We wanted to use a more rigorous formula for PD.
+# Interesting Observation
+A binary controller seems like it would do the same job here.
+This is likely due to the resolution of temperature readings
+Not being sufficient to have any sort of intermediate power states
+between fully on (1) and off (0).
 
 '''
 
@@ -33,35 +38,45 @@ def norm(v, lim):
     return max(v, -lim)
 
 class Controller(SM):
-    def __init__(self, target_temp=30.0, kp=-1.0991, kd=-0.5286):
-        self.kp = float(kp)
-        self.kd = float(kd)
+    def __init__(self, target_temp=30.0, ks=[5.69, 5.84, 4.9, 3.64]):
+        # negate ks because we're trying to DECREASE temperatur
+        self.ks = [float(-k) for k in ks]
+
         self.target_temp = target_temp
 
         self.prev_time = time()
-
+        self.es = []
         self.startState = None
 
     def getNextValues(self, state, temp):
         curr_time = time()
-        err = self.target_temp - temp
+        ks, es = self.ks, self.es
 
-        if hasattr(self, 'prev_err'):
-            prev_err = self.prev_err
-        else:
-            prev_err = err
+        e = self.target_temp - temp
+        es.append(e)
+        
+        # ensure we aren't needlessly tracking values
+        if len(es) > len(ks):
+            es.pop(0)
 
-        kp, kd, prev_time = self.kp, self.kd, self.prev_time
+        # if we don't have err values for nth terms, use old ones
+        for idx, k in enumerate(ks):
+            if len(es) <= idx:
+                es.insert(0, es[-1])
 
-        d_e = (err - prev_err)/(curr_time - prev_time)
+        power = 0.0
+        for i in range( len(ks) ):
+            power += ks[i] * es[i]
 
-        power = kp*err + kd*d_e
+        # our increments of time may not be constant
+        delta_time = curr_time - self.prev_time
+
+        power /= delta_time
         power = norm(power, 1.0)
 
         if power < 0:
             power = 0
 
-        self.prev_err  = err
         self.prev_time = curr_time
 
         return None, Powers(power, power)
